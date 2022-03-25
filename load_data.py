380efc986d46f3325039e34866c6d1d608eda8a6
wangtingshu <tings0606@gmail.com>
2022-03-25 13:58:25 -0400

    Load all data and choice of using super resolution

--- load_data.py
+++ load_data.py
@@ -3,7 +3,7 @@ import cv2 as cv
 from FaceMask import *
 import matplotlib.pyplot as plt
 import xml.etree.ElementTree as ET
-from ISR.models import RDN
+import os
 from PIL import Image
 # import torch
 
@@ -18,42 +18,37 @@ def load_image(filename):
         return image
     return None
 
-def load_annotation():
-    tree = ET.parse(annotation_path+"maksssksksss0.xml")
-    root = tree.getroot()
-    filename = root.find('filename').text
-    image = np.array(load_image(filename))
+
+def load_annotation(super_res = False):
     lst = []
-    if image is not None:
-        for index in root.iter('object'):
-            fcmsk = FaceMask()
-            fcmsk.set_mask(index.find('name').text == "with_mask")
-            xmin = int(index.find('bndbox').find('xmin').text)
-            xmax = int(index.find('bndbox').find('xmax').text)
-            ymin = int(index.find('bndbox').find('ymin').text)
-            ymax = int(index.find('bndbox').find('ymax').text)
-            cropped_image = image[ymin:ymax,xmin:xmax]
-            fcmsk.set_image(cropped_image)
-            lst.append(fcmsk)
+    sr = cv.dnn_superres.DnnSuperResImpl_create()
+    path = "models/LapSRN_x8.pb"
+    sr.readModel(path)
+    sr.setModel("lapsrn", 8)
+    for filename in os.listdir(annotation_path):
+        f = os.path.join(annotation_path, filename)
+        tree = ET.parse(f)
+        root = tree.getroot()
+        filename = root.find('filename').text
+        image = np.array(load_image(filename))
+        if image is not None:
+            for index in root.iter('object'):
+                fcmsk = FaceMask()
+                fcmsk.set_mask(index.find('name').text == "with_mask")
+                xmin = int(index.find('bndbox').find('xmin').text)
+                xmax = int(index.find('bndbox').find('xmax').text)
+                ymin = int(index.find('bndbox').find('ymin').text)
+                ymax = int(index.find('bndbox').find('ymax').text)
+                cropped_image = image[ymin:ymax,xmin:xmax]
+                if super_res:
+                    result = sr.upsample(cropped_image)
+                    fcmsk.set_image(result)
+                else:
+                    fcmsk.set_image(cropped_image)
+                    pass
+                lst.append(fcmsk)
     return lst
 
-lst = load_annotation()
 
-for i in lst:
-    plt.imshow(i.image)
-    plt.show()
-    print(i.mask)
-
-# sr = cv.dnn_superres.DnnSuperResImpl_create()
-# path = "LapSRN_x8.pb"
-# sr.readModel(path)
-# sr.setModel("lapsrn",8)
-# result = sr.upsample(lst[0].image)
-# plt.imshow(result)
-# plt.show()
-#
-
-# rdn = RDN(weights='psnr-small')
-# sr_img = rdn.predict(lst[0].image)
-# plt.imshow(sr_img)
-# plt.show()
+lst = load_annotation(super_res=True)
+print("finished")
