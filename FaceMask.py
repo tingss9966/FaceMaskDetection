65a93c295f3ec9630753deeed7828ad3bb5ecd92
wangtingshu <tings0606@gmail.com>
2022-04-18 12:53:17 -0400

    Added keras training and evaluation

--- FaceMask.py
+++ FaceMask.py
@@ -1,14 +1,5 @@
+import tensorflow
+from tensorflow import keras
+from keras import models
 
-
-class FaceMask():
-
-    def __init__(self):
-        self.image = None
-        self.mask = False
-
-    def set_image(self, image):
-        self.image = image
-
-    def set_mask(self, mask):
-        self.mask = mask
-
+model.load_weights(checkpoint_filepath)
--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import csv
 
-import keras.layers
 import numpy as np
 import matplotlib.pyplot as plt
+from tensorflow import keras
 import sklearn.model_selection as model
-import sklearn as sk
-from torchvision import models
 import torch
-from keras import applications
 from torch import nn
-import keras_efficientnet_v2
 import tensorflow as tf
-from torch.utils.data import TensorDataset, DataLoader
 
 dropout = 0.3
 arr = np.load('./imageData.npz',allow_pickle=True)
@@ -29,7 +24,7 @@ print("cuda" if torch.cuda.is_available() else "cpu")
 # print(len(x_train))
 # m = applications.efficientnetv2()
 
-model_vit = keras_efficientnet_v2.EfficientNetV2M(pretrained="imagenet")
+
 # for parameters in model_vit.parameters():
 #     parameters.requires_grad = False
 
@@ -50,92 +45,32 @@ class Net(nn.Module):
         return x
 
 
-backbone = tf.keras.applications.efficientnet_v2.EfficientNetV2M(
+backbone = tf.keras.applications.efficientnet_v2.EfficientNetV2S(
     weights = "imagenet",
     input_shape=(224,224,3),
     include_top = False
 )
 model = tf.keras.Sequential([
     backbone,
+    keras.layers.Dense(1000),
+    keras.layers.ReLU(),
+    keras.layers.Dense(500),
+    keras.layers.Dropout(dropout),
     keras.layers.GlobalAvgPool2D(),
+    keras.layers.LayerNormalization(),
     keras.layers.Dense(1)]
 )
 
 
 model.summary()
-
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),loss = tf.keras.losses.categorical_crossentropy, metrics=['accuracy'])
-temp = model.fit(x = x_train, y = y_train, validation_data = (x_val,y_val),batch_size = 30, epochs=15)
-# criterion = nn.CrossEntropyLoss()
-# optimizer = torch.optim.Adam(model.parameters(),lr=0.01)
-#
-# model.to(device)
-#
-# x_val = x_val.reshape((-1,3,224,224))
-# x_val = torch.Tensor(x_val)
-# y_val = torch.Tensor(y_val)
-# val_data = TensorDataset(x_val,y_val)
-# val_dataloader = DataLoader(val_data)
-#
-#
-# def validation(model, validateloader, criterion):
-#     val_loss = 0
-#     accuracy = 0
-#
-#     for images, labels in iter(validateloader):
-#         images, labels = images.to('cpu'), labels.to('cpu',dtype=torch.int64)
-#         images.view(images.shape[0],-1)
-#         output = model.forward(images)
-#         val_loss += criterion(output, labels).item()
-#
-#         probabilities = torch.exp(output)
-#
-#         equality = (labels.data == probabilities.max(dim=1)[1])
-#         accuracy += equality.type(torch.FloatTensor).mean()
-#
-#     return val_loss, accuracy
-#
-#
-#
-# epochs = 15
-# steps = 0
-# print_every = 40
-#
-# x_train = x_train.reshape((2850,3,224,224))
-# tensorx = torch.Tensor(x_train)
-# tensory = torch.Tensor(y_train)
-# data = TensorDataset(tensorx,tensory)
-# dataloader = DataLoader(data)
-#
-# print(x_train.shape)
-# for e in range(epochs):
-#     model.train()
-#     running_loss = 0
-#     for images, labels in iter(dataloader):
-#         images = images.to('cpu', dtype=torch.long)
-#         labels = labels.to('cpu', dtype=torch.long)
-#         steps += 1
-#         optimizer.zero_grad()
-#         print(images.shape)
-#         output = model.forward(images)
-#         print(output)
-#         loss = criterion(output.squeeze(), labels.squeeze())
-#         loss.backward()
-#         optimizer.step()
-#
-#         running_loss += loss.item()
-#
-#         model.eval()
-#
-#         # Turn off gradients for validation, saves memory and computations
-#         with torch.no_grad():
-#             validation_loss, accuracy = validation(model, val_dataloader, criterion)
-#
-#         print("Epoch: {}/{}.. ".format(e + 1, epochs),
-#               "Training Loss: {:.3f}.. ".format(running_loss / print_every),
-#               "Validation Loss: {:.3f}.. ".format(validation_loss / len(val_dataloader)),
-#               "Validation Accuracy: {:.3f}".format(accuracy / len(val_dataloader))
-#               )
-#
-#         running_loss = 0
-#         model.train()
+loss =tf.keras.losses.BinaryFocalCrossentropy()
+model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath="./weights",
+                                                               save_weights_only=True,
+                                                               monitor='val_accuracy',
+                                                               mode='max',
+                                                               save_best_only=True)
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),loss = loss, metrics=['accuracy'])
+temp = model.fit(x = x_train, y = y_train, validation_data = (x_val,y_val),batch_size = 50, epochs=2,callbacks=[model_checkpoint_callback])
+temp2 = model.evaluate(x_test, y_test, batch_size=50)
+predictions = model.predict(x_test[:3])
+print("predictions shape:", predictions.shape)
