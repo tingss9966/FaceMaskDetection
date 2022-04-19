452aff296d6fa0c70dc2e14b2ccc1087afcc99c7
wangtingshu <tings0606@gmail.com>
2022-04-19 00:12:05 -0400

    Added evaluation to best model only

--- EffnetFD.py
+++ EffnetFD.py
@@ -1,4 +1,3 @@
-
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -18,32 +17,29 @@ def get_result_plot(fitted_model):
     plt.title('Model accuracy')
     plt.xlabel('epoch')
     plt.ylabel('loss')
-    plt.legend(['train set','test set'],loc='upper right')
+    plt.legend(['train set', 'test set'], loc='upper right')
     plt.show()
 
 
 # Hyper-parameters
 lr = 0.001
-epochs = 15
+epochs = 30
 dropout = 0.3
 batch_size = 50
 
-arr = np.load('./imageData.npz',allow_pickle=True)
+arr = np.load('./imageData.npz', allow_pickle=True)
 img_list = arr['arr_0']
 annotation_list = arr['arr_1']
 
-x_train,x_tot,y_train,y_tot = model.train_test_split(img_list,annotation_list,test_size=0.3,random_state=42)
-x_val,x_test,y_val,y_test = model.train_test_split(x_tot,y_tot,test_size=0.5,random_state=42)
+x_train, x_tot, y_train, y_tot = model.train_test_split(img_list, annotation_list, test_size=0.3, random_state=42)
+x_val, x_test, y_val, y_test = model.train_test_split(x_tot, y_tot, test_size=0.5, random_state=42)
 
-device = torch.device("cuda" if torch.cuda.is_available()else "cpu")
-
-if exists("./weights.h5"):
-    print("hello")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 backbone = tf.keras.applications.efficientnet_v2.EfficientNetV2S(
-    weights = "imagenet",
-    input_shape=(224,224,3),
-    include_top = False
+    weights="imagenet",
+    input_shape=(224, 224, 3),
+    include_top=False
 )
 model = tf.keras.Sequential([
     backbone,
@@ -56,29 +52,28 @@ model = tf.keras.Sequential([
     keras.layers.Dense(1)]
 )
 
-
 model.summary()
-loss =tf.keras.losses.BinaryFocalCrossentropy()
+loss = tf.keras.losses.BinaryFocalCrossentropy()
 model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath="./effnet_weights.h5",
-                                                               save_weights_only=True,
                                                                monitor='val_accuracy',
                                                                mode='max',
                                                                save_best_only=True)
 
 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr),
-              loss = loss,
+              loss=loss,
               metrics=['accuracy'])
 
-temp = model.fit(x = x_train,
-                 y = y_train,
-                 validation_data = (x_val,y_val),
-                 batch_size = batch_size,
+temp = model.fit(x=x_train,
+                 y=y_train,
+                 validation_data=(x_val, y_val),
+                 batch_size=batch_size,
                  epochs=epochs,
                  callbacks=[model_checkpoint_callback])
-model.save("effnet_weights.h5")
 
-temp2 = model.evaluate(x_test,
-                       y_test,
-                       batch_size=batch_size)
+best_model = keras.models.load_model("effnet_weights.h5")
 
-get_result_plot(model)
+temp2 = best_model.evaluate(x_test,
+                            y_test,
+                            batch_size=batch_size)
+
+get_result_plot(temp)
--- videoFD.py
+++ videoFD.py
@@ -1,25 +1,31 @@
-import cv2
+import cv2 as cv
 import numpy as np
 from tensorflow import keras
 
 labels_dict={0:'without_mask',1:'with_mask'}
-face_clsfr=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_clsfr=cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 size = 4
 load_model = keras.models.load_model('./effnet_weights.h5')
-cv2.namedWindow("Face Mask Detection")
-cam = cv2.VideoCapture(0)
+cv.namedWindow("Face Mask Detection")
+cam = cv.VideoCapture(0)
 color_dict={0:(0,0,255),1:(0,255,0)}
 
-classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+sr = cv.dnn_superres.DnnSuperResImpl_create()
+path = "models/LapSRN_x8.pb"
+sr.readModel(path)
+sr.setModel("lapsrn", 8)
+
+classifier = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 while True:
     rval, image = cam.read()
-    image=cv2.flip(image,1,1)
-    small = cv2.resize(image, (image.shape[1] // size, image.shape[0] // size))
+    image=cv.flip(image,1,1)
+    small = cv.resize(image, (image.shape[1] // size, image.shape[0] // size))
     faces = classifier.detectMultiScale(small)
     for f in faces:
         (x, y, w, h) = [v * size for v in f]
         face_img = image[y:y + h, x:x + w]
-        resized = cv2.resize(face_img, (224, 224))
+        # face_img = sr.upsample(face_img)
+        resized = cv.resize(face_img, (224, 224))
         reshaped = np.reshape(resized, (1, 224, 224, 3))
         reshaped = np.vstack([reshaped])
         result = load_model.predict(reshaped)
@@ -31,15 +37,15 @@ while True:
             label = 0
         else:
             label = 1
-        cv2.rectangle(image, (x, y), (x + w, y + h), color_dict[label], 2)
-        cv2.rectangle(image, (x, y - 40), (x + w, y), color_dict[label], -1)
-        cv2.putText(image, labels_dict[label], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8,
+        cv.rectangle(image, (x, y), (x + w, y + h), color_dict[label], 2)
+        cv.rectangle(image, (x, y - 40), (x + w, y), color_dict[label], -1)
+        cv.putText(image, labels_dict[label], (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.8,
                     (255, 255, 255), 2)
 
     if image is not None:
-        cv2.imshow('Face Mask Detection', image)
-    key = cv2.waitKey(10)
+        cv.imshow('Face Mask Detection', image)
+    key = cv.waitKey(10)
     if key == 27:
         break
 cam.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
